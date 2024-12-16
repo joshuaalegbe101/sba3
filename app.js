9943a5aca3a96faf56254adebc2bc67e4897e24c
joshuaalegbe101 <joshuaalegbe101@gmail.com>
2024-12-15 18:36:34 -0600

    Event listener for WASD - beg

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+const player = document.getElementById("player");
+const gameContainner = document.getElementById("gameContainer")
+const enemy = document.getElementById("enemy");
+
+let playerPosition = 
+{ 
+    x:50, 
+    y:50
+};
+
+document.addEventListener("keydown", (evt) => {
+    const containerBounds = gameContainner.getBoundingClientRect();
+    const playerBounds = player.getBoundingClientRect();
+
+
+});
+
+
+
+
+
+
+/*
 selectElementByID()
 querySelector()
 
@@ -14,4 +37,5 @@ addEventListener()
 BOM
 
 BOM 
+*/
 
