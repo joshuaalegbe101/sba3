85e5e5a456294ba9615abe3f5509590b6ba85f8f
joshuaalegbe101 <joshuaalegbe101@gmail.com>
2024-12-15 21:40:51 -0600

    Enemy movement done

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const player = document.getElementById("player");
 const gameContainer = document.getElementById("gameContainer")
-const enemy = document.getElementById("enemy");
+const enemy = document.getElementById("alien");
 
 let playerPosition = { x:5, y:5};
 let enemyVelocity = {x:3, y:3};
-let enemyPosition = {x :150, y:170};
+let enemyPosition = {x :15, y:100};
 
 
 document.addEventListener("keydown", (evt) => {
@@ -30,19 +30,33 @@ document.addEventListener("keydown", (evt) => {
 });
 
 function moveEnemy() {
-    const containerBounds = gameContainer.getBoundingClientRect();
-    const enemyBounds = enemy.getBoundingClientRect();
+    const containerStyle = getComputedStyle(gameContainer);
+    const borderWidth = parseInt(containerStyle.borderWidth) || 0;
+    const containerWidth = gameContainer.offsetWidth - borderWidth *2;
+    const containerHeight = gameContainer.offsetHeight - borderWidth *2;
 
     enemyPosition.x += enemyVelocity.x;
     enemyPosition.y += enemyVelocity.y;
 
-    if(enemyBounds.right >= containerBounds.right || enemyBounds.left <= containerBounds.left) 
+    if (enemyPosition.x + enemy.offsetWidth >= containerWidth) {
         enemyVelocity.x *= -1;
+        enemyPosition.x = containerWidth - enemy.offsetWidth; 
+    }
+    if (enemyPosition.x <= 0) {
+        enemyVelocity.x *= -1;
+        enemyPosition.x = 0;
+    }
 
-    if(enemyBounds.bottom >= containerBounds.bottom || enemyBounds.top <= containerBounds.top)
+    if (enemyPosition.y + enemy.offsetHeight >= containerHeight) {
         enemyVelocity.y *= -1;
+        enemyPosition.y = containerHeight - enemy.offsetHeight; 
+    }
+    if (enemyPosition.y <= 0) {
+        enemyVelocity.y *= -1;
+        enemyPosition.y = 0; 
+    }
 
-    enemy.style.tranform = `translate(${enemyPosition.x}px, ${enemyPosition.y}px`;
+    enemy.style.transform = `translate(${enemyPosition.x}px, ${enemyPosition.y}px)`;
 
     requestAnimationFrame(moveEnemy)
 }
